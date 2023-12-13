8a528b9bc22bf5eafc9eaaee5200784454a2e710
ruthcaswellsmith <ruth@plannuh.com>
2023-12-13 17:58:04 -0500

    Day13

--- Day13.py
+++ Day13.py
@@ -4,23 +4,39 @@ import numpy as np
 from enum import Enum, auto
 
 
+def parse_input(data: List[str]):
+    empty_list_indices = [i for i, sublist in enumerate(data) if not sublist]
+    ind = 0
+    grids = []
+    for i in empty_list_indices:
+        grids.append(data[ind:i])
+        ind = i + 1
+    grids.append(data[ind:])
+
+    grid_arrays = []
+    for grid in grids:
+        grid_arrays.append(np.array([[1 if ele == '#' else 0 for ele in line] for line in grid]))
+
+    return grid_arrays
+
+
 class Direction(str, Enum):
     HORIZONTAL = auto()
     VERTICAL = auto()
 
 
 class Pattern:
-    def __init__(self, num: int, data: List[List[str]]):
+    def __init__(self, num: int, pattern: np.array):
         self.num = num
-        self.pattern = np.array([[1 if ele == '#' else 0 for ele in line] for line in data])
+        self.pattern = pattern
         self.horizontal_ind = self.find_relected_line(Direction.HORIZONTAL)
         self.vertical_ind = self.find_relected_line(Direction.VERTICAL) if self.horizontal_ind is None else None
 
     @property
     def score(self):
-        if self.vertical_ind:
+        if self.vertical_ind != None:
             return self.vertical_ind + 1
-        if self.horizontal_ind:
+        if self.horizontal_ind != None:
             return 100 * (self.horizontal_ind + 1)
 
     def pattern_reflects(self, array: np.array, ind: int):
@@ -53,16 +69,31 @@ class Pattern:
 
 
 if __name__ == '__main__':
-    filename = 'input/test.txt'
+    filename = 'input/Day13.txt'
     data = read_file(filename)
+    grids = parse_input(data)
 
-    empty_list_indices = [i for i, sublist in enumerate(data) if not sublist]
-    ind = 0
-    grids = []
-    for i in empty_list_indices:
-        grids.append(data[ind:i])
-        ind = i + 1
-    grids.append(data[ind:])
-
-    patterns = [Pattern(num, line) for num, line in enumerate(grids)]
+    patterns = [Pattern(num, grid) for num, grid in enumerate(grids)]
     print(f"The answer to part 1 is {sum([pattern.score for pattern in patterns])}.")
+
+    counts = {}
+    for pattern in patterns:
+        count = 0
+
+        array = pattern.pattern
+        for i in range(array.shape[0]):
+            for j in range(i + 1, array.shape[0]):
+                if sum(array[i, :] != array[j, :]) == 1:
+                    count += 1
+
+        array = np.rot90(pattern.pattern, k=3)
+        for i in range(array.shape[0]):
+            for j in range(i + 1, array.shape[0]):
+                if sum(array[i, :] != array[j, :]) == 1:
+                    count += 1
+
+        counts[pattern.num] = count
+    print(1)
+
+
+
