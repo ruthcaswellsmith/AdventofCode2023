f7b9a4d445d3c9518d604ac7f7a4f4b18e58ff0a
ruthcaswellsmith <ruth@plannuh.com>
2023-12-12 20:13:25 -0500

    Day12

--- Day12.py
+++ Day12.py
@@ -2,6 +2,8 @@ from utils import read_file
 import re
 from typing import List
 from functools import lru_cache
+from itertools import takewhile
+
 
 NUM_FOLDS = 5
 
@@ -11,10 +13,11 @@ class Record:
         self.springs = springs
         self.groups_to_find = groups_to_find
         self.pattern = re.compile(self.get_pattern())
+        self.multiple_dots = re.compile(r'\.+')
         self.matches = self.find_matches(self.springs)
 
     def get_pattern(self) -> str:
-        pound_signs = ["".join(['#' for i in range(group)]) for group in self.groups_to_find]
+        pound_signs = ["".join(['#' for _ in range(group)]) for group in self.groups_to_find]
         pattern = "^[.]*"
         for pound_sign in pound_signs:
             pattern += pound_sign + "[.]+"
@@ -29,13 +32,7 @@ class Record:
         return groups
 
     @lru_cache
-    def find_matches(self, string: str, memo=None):
-        if memo is None:
-            memo = {}
-
-        if string in memo:
-            return memo[string]
-
+    def find_matches(self, string: str):
         # If we have a complete string check if it matches
         if string.find('?') == -1:
             if self.pattern.match(string):
@@ -43,22 +40,26 @@ class Record:
             else:
                 return []
 
-        # Otherwise check if this is even a possibility
-        groups = self.groups(string[:string.find('?')])
-        if groups:
-            if len(groups) > len(self.groups_to_find) or \
-                    groups[-1] > self.groups_to_find[len(groups)-1]:
+        # First check we don't have too many groups
+        groups = [group for group in self.multiple_dots.split(string) if group]
+        definite_groups = [group for group in groups if '#' in group]
+        if len(definite_groups) > len(self.groups_to_find):
+            return []
+
+        # Now check that the groups we have so far match
+        known_groups = list(takewhile(lambda x: '?' not in x, groups))
+        if known_groups:
+            if len(known_groups[-1]) > self.groups_to_find[len(known_groups)-1]:
                 return []
         results = []
         for char in ['#', '.']:
             new_str = string.replace('?', char, 1)
-            results += self.find_matches(new_str, memo)
-            memo[new_str] = results
+            results += self.find_matches(new_str)
         return results
 
 
 if __name__ == '__main__':
-    filename = 'input/test.txt'
+    filename = 'input/Day12.txt'
     data = read_file(filename)
 
     records = []
