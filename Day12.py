2618a098cc3a8ab624913c418e04c1e8d360eed0
ruthcaswellsmith <ruth@plannuh.com>
2023-12-15 12:54:28 -0500

    Day14

--- Day12.py
+++ Day12.py
@@ -14,6 +14,7 @@ class Record:
         self.groups_to_find = groups_to_find
         self.pattern = re.compile(self.get_pattern())
         self.multiple_dots = re.compile(r'\.+')
+        print(f"New Record {self.groups_to_find}\n")
         self.matches = self.find_matches(self.springs)
 
     def get_pattern(self) -> str:
@@ -33,6 +34,7 @@ class Record:
 
     @lru_cache
     def find_matches(self, string: str):
+        print(string)
         # If we have a complete string check if it matches
         if string.find('?') == -1:
             if self.pattern.match(string):
@@ -41,13 +43,16 @@ class Record:
                 return []
 
         # First check we don't have too many groups
-        groups = [group for group in self.multiple_dots.split(string) if group]
+        groups = [group for group in self.multiple_dots.split(string) if group != ""]
         definite_groups = [group for group in groups if '#' in group]
+        unknown_groups = [group for group in groups if '#' not in group]
         if len(definite_groups) > len(self.groups_to_find):
+            # or \
+            #     len(definite_groups) + len(unknown_groups) < len(self.groups_to_find):
             return []
 
         # Now check that the groups we have so far match
-        known_groups = list(takewhile(lambda x: '?' not in x, groups))
+        known_groups = re.findall('#+', string[:string.find('?')])
         if known_groups:
             if len(known_groups[-1]) > self.groups_to_find[len(known_groups)-1]:
                 return []
@@ -59,7 +64,7 @@ class Record:
 
 
 if __name__ == '__main__':
-    filename = 'input/Day12.txt'
+    filename = 'input/test.txt'
     data = read_file(filename)
 
     records = []
@@ -71,11 +76,12 @@ if __name__ == '__main__':
     matches = [record.matches for record in records]
     print(f"The answer to part 1 is {sum([len(match) for match in matches])}.")
 
-    new_records = []
-    for i, record in enumerate(records):
-        new_groups_to_find = record.groups_to_find * NUM_FOLDS
-        new_springs = "?".join([record.springs for _ in range(NUM_FOLDS)])
-        new_records.append(Record(new_springs, new_groups_to_find))
-        print(i, len(new_records[-1].matches))
-    matches = [record.find_matches(record.springs) for record in new_records]
-    print(f"The answer to part 2 is {sum([len(match) for match in matches])}.")
+#     new_records = []
+#     for i, record in enumerate(records):
+#         new_groups_to_find = record.groups_to_find * NUM_FOLDS
+#         new_springs = "?".join([record.springs for _ in range(NUM_FOLDS)])
+#         new_records.append(Record(new_springs, new_groups_to_find))
+#         print(i, len(new_records[-1].matches))
+#     matches = [record.find_matches(record.springs) for record in new_records]
+#     print(f"The answer to part 2 is {sum([len(match) for match in matches])}.")
+# matches
